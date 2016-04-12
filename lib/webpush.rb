2595b86ccde38f7298323ed247a8bd2872a82c61
zaru <sakuraba@basicinc.jp>
2016-04-12 23:59:04 +0900

    supported firefox

--- lib/webpush.rb
+++ lib/webpush.rb
@@ -12,31 +12,34 @@ module Webpush
   TEMP_GCM_URL = 'https://gcm-http.googleapis.com/gcm'
 
   class << self
-    def payload_send(message:, endpoint:, p256dh:, auth:, api_key:)
+    def payload_send(message:, endpoint:, p256dh:, auth:, api_key: "")
       endpoint = endpoint.gsub(GCM_URL, TEMP_GCM_URL)
       p256dh = unescape_base64(p256dh)
       auth = unescape_base64(auth)
 
       payload = encrypt(message, p256dh, auth)
-      gcm_post(endpoint, payload, api_key)
+      push_server_post(endpoint, payload, api_key)
     end
 
     private
 
-    def gcm_post(endpoint, payload, api_key)
+    def push_server_post(endpoint, payload, api_key = "")
       begin
         uri = URI.parse(endpoint)
         http = Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         header = {
-          "Encryption" => "salt=#{Base64.urlsafe_encode64(payload[:salt]).delete('=')}",
-          "Crypto-Key" => "dh=#{Base64.urlsafe_encode64(payload[:server_public_key_bn]).delete('=')}",
-          "Authorization" => "key=#{api_key}"
+            "Content-Type" => "application/octet-stream",
+            "Content-Encoding" => "aesgcm",
+            "Encryption" => "salt=#{Base64.urlsafe_encode64(payload[:salt]).delete('=')}",
+            "Crypto-Key" => "dh=#{Base64.urlsafe_encode64(payload[:server_public_key_bn]).delete('=')}"
         }
+        header["Authorization"] = "key=#{api_key}" unless api_key.empty?
         req = Net::HTTP::Post.new(uri.request_uri, header)
         req.body = payload[:ciphertext]
         res = http.request(req)
+        p res.body
         return ("201" == res.code) ? true : false
       rescue
         return false
