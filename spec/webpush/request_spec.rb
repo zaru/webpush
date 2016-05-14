a8d4aecc15a5d03681c4b2636bbac1a7bee503e4
Ross Kaffenberger <rosskaff@gmail.com>
2016-05-14 17:47:12 -0400

    Test for optional :ttl
    Test for optional :ttl
    
    Typo

--- spec/webpush/request_spec.rb
+++ spec/webpush/request_spec.rb
@@ -7,38 +7,56 @@ describe Webpush::Request do
     it { expect(request.headers['Content-Type']).to eq('application/octet-stream') }
     it { expect(request.headers['Ttl']).to eq('2419200') }
 
-    it 'inserts encryption headers for valid payload' do
-      payload = {
-        ciphertext: "ciphertext",
-        server_public_key_bn:
-        "server_public_key_bn",
-        salt: "salt"
-      }
-      request = Webpush::Request.new("endpoint", payload: payload)
+    describe 'from :payload' do
+      it 'inserts encryption headers for valid payload' do
+        payload = {
+          ciphertext: "ciphertext",
+          server_public_key_bn:
+          "server_public_key_bn",
+          salt: "salt"
+        }
+        request = Webpush::Request.new("endpoint", payload: payload)
 
-      expect(request.headers['Content-Encoding']).to eq("aesgcm")
-      expect(request.headers['Encryption']).to eq("salt=c2FsdA")
-      expect(request.headers['Crypto-Key']).to eq("dh=c2VydmVyX3B1YmxpY19rZXlfYm4")
+        expect(request.headers['Content-Encoding']).to eq("aesgcm")
+        expect(request.headers['Encryption']).to eq("salt=c2FsdA")
+        expect(request.headers['Crypto-Key']).to eq("dh=c2VydmVyX3B1YmxpY19rZXlfYm4")
+      end
     end
 
-    it 'inserts Authorization header when api_key present' do
-      request = Webpush::Request.new("endpoint", api_key: "api_key")
+    describe 'from :api_key' do
+      it 'inserts Authorization header when api_key present' do
+        request = Webpush::Request.new("endpoint", api_key: "api_key")
 
-      expect(request.headers['Authorization']).to eq("key=api_key")
+        expect(request.headers['Authorization']).to eq("key=api_key")
+      end
+
+      it 'does not insert Authorization header when api_key blank' do
+        request = Webpush::Request.new("endpoint", api_key: nil)
+
+        expect(request.headers['Authorization']).to be_nil
+
+        request = Webpush::Request.new("endpoint", api_key: "")
+
+        expect(request.headers['Authorization']).to be_nil
+
+        request = Webpush::Request.new("endpoint")
+
+        expect(request.headers['Authorization']).to be_nil
+      end
     end
 
-    it 'does not insert Authorization header when api_key blank' do
-      request = Webpush::Request.new("endpoint", api_key: nil)
+    describe 'from :ttl' do
+      it 'can override Ttl with :ttl option with string' do
+        request = Webpush::Request.new("endpoint", ttl: '300')
 
-      expect(request.headers['Authorization']).to be_nil
+        expect(request.headers['Ttl']).to eq('300')
+      end
 
-      request = Webpush::Request.new("endpoint", api_key: "")
+      it 'can override Ttl with :ttl option with fixnum' do
+        request = Webpush::Request.new("endpoint", ttl: 60 * 5)
 
-      expect(request.headers['Authorization']).to be_nil
-
-      request = Webpush::Request.new("endpoint")
-
-      expect(request.headers['Authorization']).to be_nil
+        expect(request.headers['Ttl']).to eq('300')
+      end
     end
   end
 end
