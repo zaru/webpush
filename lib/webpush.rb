8f934404422828b22efe9cf439bb7c937ff8f4e4
Ross Kaffenberger <rosskaff@gmail.com>
2016-05-15 08:05:45 -0400

    Document Webpush#payload_send

--- lib/webpush.rb
+++ lib/webpush.rb
@@ -15,6 +15,17 @@ module Webpush
   TEMP_GCM_URL = 'https://gcm-http.googleapis.com/gcm'
 
   class << self
+    # Deliver the payload to the required endpoint given by the JavaScript
+    # PushSubscription. Including an optional message requires p256dh and
+    # auth keys from the PushSubscription.
+    #
+    # @param endpoint [String] the required PushSubscription url
+    # @param message [String] the optional payload
+    # @param p256dh [String] the user's public ECDH key given by the PushSubscription
+    # @param auth [String] the user's private ECDH key given by the PushSubscription
+    # @param options [Hash<Symbol,String>] additional options for the notification
+    # @option options [String] :api_key required for Google, omit for Firefox
+    # @option options [#to_s] :ttl Time-to-live in seconds
     def payload_send(endpoint:, message: "", p256dh: "", auth: "", **options)
       endpoint = endpoint.gsub(GCM_URL, TEMP_GCM_URL)
 
