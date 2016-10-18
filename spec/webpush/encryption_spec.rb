1a54866f98017ce43e50dc21c1ddb246b90312a5
Ross Kaffenberger <rosskaff@gmail.com>
2016-10-18 15:03:30 -0400

    Implement fix for base64 padding for subscription keys if necessary

--- spec/webpush/encryption_spec.rb
+++ spec/webpush/encryption_spec.rb
@@ -39,6 +39,24 @@ describe Webpush::Encryption do
       expect{Webpush::Encryption.encrypt("Hello world", p256dh, nil)}.to raise_error(ArgumentError)
     end
 
+    # Bug fix for https://github.com/zaru/webpush/issues/22
+    it 'handles unpadded base64 encoded subscription keys' do
+      unpadded_p256dh = "BK74n-ZA6kfMDEuCFbQH1Y5T33p39PvnzNeuD5LqTs8cF-uaQFUHn_v5kwV6dYIIL4nFabxghQNF_vlnAXX7OiU"
+      unpadded_auth = "1C1PBkJQsVwD9tkuLR1x5A"
+
+      payload = Webpush::Encryption.encrypt("Hello World", unpadded_p256dh, unpadded_auth)
+      encrypted = payload.fetch(:ciphertext)
+
+      decrypted_data = ECE.decrypt(encrypted,
+        key: payload.fetch(:shared_secret),
+        salt: payload.fetch(:salt),
+        server_public_key: payload.fetch(:server_public_key_bn),
+        user_public_key: decode64(pad64(unpadded_p256dh)),
+        auth: decode64(pad64(unpadded_auth)))
+
+      expect(decrypted_data).to eq("Hello World")
+    end
+
     def generate_ecdh_key
       group = "prime256v1"
       curve = OpenSSL::PKey::EC.new(group)
@@ -50,8 +68,15 @@ describe Webpush::Encryption do
       Base64.urlsafe_encode64(bytes)
     end
 
-    def decode64(bytes)
-      Base64.urlsafe_decode64(bytes)
+    def decode64(str)
+      Base64.urlsafe_decode64(str)
+    end
+
+    def pad64(str)
+      if !str.end_with?("=") && str.length % 4 != 0
+        str = str.ljust((str.length + 3) & ~3, "=")
+      end
+      str
     end
   end
 end
