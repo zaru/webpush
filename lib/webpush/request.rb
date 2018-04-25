750d03f9cd851c4e8be780f6c6a86f8bd3e2f707
Alexandr Borisov <ab@cifronomika.ru>
2018-04-25 12:44:59 +0300

    add http timeout options

--- lib/webpush/request.rb
+++ lib/webpush/request.rb
@@ -18,6 +18,10 @@ module Webpush
     def perform
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
+      http.ssl_timeout = @options[:ssl_timeout] unless @options[:ssl_timeout].nil?
+      http.open_timeout = @options[:open_timeout] unless @options[:open_timeout].nil?
+      http.read_timeout = @options[:read_timeout] unless @options[:read_timeout].nil?
+
       req = Net::HTTP::Post.new(uri.request_uri, headers)
       req.body = body
       resp = http.request(req)
