125ed10c0fd56e3abc4045914fcadb6cafd3a8dc
mohamed <mohamed.m.m.hafez@gmail.com>
2019-07-28 21:24:24 -0700

    switch to using aes128gcm (based on @mplatov's work)

--- lib/webpush/encryption.rb
+++ lib/webpush/encryption.rb
@@ -23,50 +23,39 @@ module Webpush
 
       client_auth_token = Webpush.decode64(auth)
 
-      prk = HKDF.new(shared_secret, salt: client_auth_token, algorithm: 'SHA256', info: "Content-Encoding: auth\0").next_bytes(32)
+      info =  "WebPush: info\0" + convert16bit(client_public_key_bn) + convert16bit(server_public_key_bn)
+      keyinfo = "Content-Encoding: aes128gcm\0"
+      nonce_info = "Content-Encoding: nonce\0"
 
-      context = create_context(client_public_key_bn, server_public_key_bn)
+      prk = HKDF.new(shared_secret, salt: client_auth_token, algorithm: 'SHA256', info: info).next_bytes(32)
 
-      content_encryption_key_info = create_info('aesgcm', context)
-      content_encryption_key = HKDF.new(prk, salt: salt, info: content_encryption_key_info).next_bytes(16)
-
-      nonce_info = create_info('nonce', context)
+      content_encryption_key = HKDF.new(prk, salt: salt, info: keyinfo).next_bytes(16)
       nonce = HKDF.new(prk, salt: salt, info: nonce_info).next_bytes(12)
 
       ciphertext = encrypt_payload(message, content_encryption_key, nonce)
 
       {
-        ciphertext: ciphertext,
-        salt: salt,
-        server_public_key_bn: convert16bit(server_public_key_bn),
-        server_public_key: server_public_key_bn.to_s(2),
-        shared_secret: shared_secret
+          ciphertext: ciphertext,
+          salt: salt,
+          server_public_key_bn: convert16bit(server_public_key_bn),
+          server_public_key: server_public_key_bn.to_s(2),
+          shared_secret: shared_secret
       }
     end
     # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
     private
 
-    def create_context(client_public_key, server_public_key)
-      c = convert16bit(client_public_key)
-      s = convert16bit(server_public_key)
-      context = "\0"
-      context += [c.bytesize].pack('n*')
-      context += c
-      context += [s.bytesize].pack('n*')
-      context += s
-      context
-    end
-
     def encrypt_payload(plaintext, content_encryption_key, nonce)
       cipher = OpenSSL::Cipher.new('aes-128-gcm')
       cipher.encrypt
       cipher.key = content_encryption_key
       cipher.iv = nonce
-      padding = cipher.update("\0\0")
-      text = cipher.update(plaintext)
 
-      e_text = padding + text + cipher.final
+      text = cipher.update(plaintext)
+      padding = cipher.update("\2\0")
+      e_text = text + padding + cipher.final
+
       e_tag = cipher.auth_tag
 
       e_text + e_tag
