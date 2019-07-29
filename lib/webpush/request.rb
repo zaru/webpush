19f084102f4e7b55e8f692525222b0cf3da0e532
mohamed <mohamed.m.m.hafez@gmail.com>
2019-07-29 07:41:41 -0700

    put Authorization header back

--- lib/webpush/request.rb
+++ lib/webpush/request.rb
@@ -48,6 +48,10 @@ module Webpush
 
       if api_key?
         headers['Authorization'] = "key=#{api_key}"
+      elsif vapid?
+        vapid_headers = build_vapid_headers
+        headers['Authorization'] = vapid_headers['Authorization']
+        headers['Crypto-Key'] = [headers['Crypto-Key'], vapid_headers['Crypto-Key']].compact.join(';')
       end
 
       headers
