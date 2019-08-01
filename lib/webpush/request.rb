faac3a1cea1e10e788b0bc0e68c07dd4b4061981
mohamed <mohamed.m.m.hafez@gmail.com>
2019-07-31 21:16:13 -0700

    put back Vapid from pem, which was mistakenly removed

--- lib/webpush/request.rb
+++ lib/webpush/request.rb
@@ -57,7 +57,7 @@ module Webpush
     def build_vapid_header
       # https://tools.ietf.org/id/draft-ietf-webpush-vapid-03.html
 
-      vapid_key = VapidKey.from_keys(vapid_public_key, vapid_private_key)
+      vapid_key = vapid_pem ? VapidKey.from_pem(vapid_pem) : VapidKey.from_keys(vapid_public_key, vapid_private_key)
       jwt = JWT.encode(jwt_payload, vapid_key.curve, 'ES256', jwt_header_fields)
       p256ecdsa = vapid_key.public_key_for_push_header
 
